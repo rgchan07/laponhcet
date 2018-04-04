3521bc91710648c72f3e5de5750fcb2be13e84d8
RichardChan <RichardChan@ISD-PF0ZH50A.local>
2018-04-04 14:40:23 +0800

    Student List

--- LaponhcetWeb/src/com/laponhcet/util/StudentUtil.java
+++ LaponhcetWeb/src/com/laponhcet/util/StudentUtil.java
@@ -164,3 +164,165 @@ public class StudentUtil implements Serializable {
 	
 	*/
 }
+package com.laponhcet.util;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.laponhcet.dao.StudentDAO;
+import com.laponhcet.dto.AcademicProgramDTO;
+import com.laponhcet.dto.StudentDTO;
+import com.mytechnopal.Pagination;
+import com.mytechnopal.SessionInfo;
+import com.mytechnopal.base.DTOBase;
+import com.mytechnopal.dao.UserDAO;
+import com.mytechnopal.dto.UserDTO;
+import com.mytechnopal.util.DTOUtil;
+import com.mytechnopal.util.StringUtil;
+
+public class StudentUtil implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	/*public static void setPaginationRecord(SessionInfo sessionInfo, Pagination pagination, List<DTOBase> academicProgramList) {
+		for(DTOBase dto: pagination.getCurrentPageRecordList()) {
+			StudentDTO student = (StudentDTO) dto;
+			UserDTO user = new UserDAO().getUserByCode(student.getCode());
+			//AcademicProgramDTO academicProgram = (AcademicProgramDTO) DTOUtil.getObjByCode(academicProgramList, student.getAcademicProgram().getCode());
+			String profilePict = StringUtil.isEmpty(user.getProfilePict())?UserDTO.PROFILE_PICT_DEFAULT:user.getProfilePict();
+			student.setPaginationRecord(new String[]{"<img width='200px' height='200px' src='" + profilePict + "'>", student.getAcademicProgram().getCode(), user.getCode(), user.getLastName(), user.getFirstName(), user.getMiddleName(), pagination.getLinkButtonStr(sessionInfo, student.getId()).replace("~", ",")});
+		}
+	}*/
+	
+	public static void setPaginationRecord(SessionInfo sessionInfo, Pagination pagination, List<DTOBase> academicProgramList) {
+		for(DTOBase dto: pagination.getCurrentPageRecordList()) {
+			StudentDTO student = (StudentDTO) dto;
+			UserDTO user = new UserDAO().getUserByCode(student.getCode());
+			String profilePict = StringUtil.isEmpty(user.getProfilePict())?UserDTO.PROFILE_PICT_DEFAULT:user.getProfilePict();
+			
+			StringBuffer strBuff = new StringBuffer();	
+			
+			strBuff.append("<div class='contact-box center-version'>");
+			strBuff.append("	<img alt='image' class='img-circle' src='" + profilePict + "'>");
+			strBuff.append("	<h3 class='m-b-xs'><strong>" + user.getName(false, true, false) + "</strong></h3>");
+			strBuff.append("	<div class='font-bold'>Graphics designer</div>");
+			strBuff.append("    <address class='m-t-md'>");
+			strBuff.append("       	<strong>" + user.getCityPermanent().getName() + "</strong><br>");
+			strBuff.append(		   	user.getStreetPermanent() + "<br>");
+			strBuff.append(			user.getBarangayPermanent() + "<br>");
+			strBuff.append("       	<i class=\"fa fa-mobile-phone\"></i>" + user.getCpNumber());
+			strBuff.append("    </address>");
+			strBuff.append("	<div class='contact-box-footer'>");
+			strBuff.append(			pagination.getLinkButtonStr(sessionInfo, student.getId()).replace("~", ","));
+			strBuff.append("	</div>");
+			strBuff.append("</div>");
+
+			
+			student.setPaginationRecord(new String[]{strBuff.toString()});
+		}
+	}
+	
+	public static List<DTOBase> getStudentListByUserList(Pagination pagination, List<DTOBase> userList, List<DTOBase> academicProgramList){
+		List<DTOBase> resultList = new ArrayList<DTOBase>();
+		for(DTOBase userObj : userList){
+			UserDTO user = (UserDTO) userObj;
+			StudentDTO student = setStudent((StudentDTO) DTOUtil.getObjByCode(pagination.getRecordListUnfiltered(), user.getCode()), user, academicProgramList);
+			resultList.add(student);
+		}
+		return resultList;
+	}
+	
+	public static StudentDTO setStudent(StudentDTO student, UserDTO user, List<DTOBase> academicProgramList){
+		student.setProfilePict(user.getProfilePict());
+		student.setRfid(user.getRfid());
+		student.setFacebookId(user.getFacebookId());
+		student.setAcademicProgram((AcademicProgramDTO) DTOUtil.getObjByCode(academicProgramList, student.getAcademicProgram().getCode()));
+		student.setCpNumber(user.getCpNumber());
+		student.setLandlineNumber(user.getLandlineNumber());
+		student.setEmailAddress(user.getEmailAddress());
+		student.setPrefixName(user.getPrefixName());
+		student.setLastName(user.getLastName());
+		student.setFirstName(user.getFirstName());
+		student.setMiddleName(user.getMiddleName());
+		student.setSuffixName(user.getSuffixName());
+		student.setStreetPermanent(user.getStreetPermanent());
+		student.setBarangayPermanent(user.getBarangayPermanent());
+		student.setCityPermanent(user.getCityPermanent());
+		student.setStreetPresent(user.getStreetPresent());
+		student.setBarangayPresent(user.getBarangayPresent());
+		student.setCityPresent(user.getCityPresent());
+		student.setBirthPlace(user.getBirthPlace());
+		student.setBirthDate(user.getBirthDate());
+		student.setGender(user.getGender());
+		student.setReligion(user.getReligion());
+		student.setMaritalStatus(user.getMaritalStatus());
+		student.setCitizenship(user.getCitizenship());
+		student.setPassportNumber(user.getPassportNumber());
+		student.setFatherName(user.getFatherName());
+		student.setFatherOccupation(user.getFatherOccupation());
+		student.setFatherCpNumber(user.getFatherCpNumber());
+		student.setMotherName(user.getMotherName());
+		student.setMotherOccupation(user.getOccupation());
+		student.setMotherCpNumber(user.getMotherCpNumber());
+		student.setGuardianName(user.getGuardianName());
+		student.setGuardianOccupation(user.getGuardianOccupation());
+		student.setGuardianRelation(user.getGuardianRelation());
+		student.setContactPerson(user.getContactPerson());
+		student.setContactRelation(user.getContactRelation());
+		student.setContactAddress(user.getContactAddress());
+		student.setContactCPNumber(user.getContactCPNumber());
+		student.setContactLandlineNumber(user.getContactLandlineNumber());
+		student.setContactEmailAddress(user.getContactEmailAddress());
+		student.setContactFacebookId(user.getContactRelation());
+		return student;
+	}
+	
+	/*
+	
+	public static List<DTOBase> getStudentListByCodeName(List<DTOBase> userListByCodeName, List<DTOBase> studentList){
+		List<DTOBase> newStudentList = new ArrayList<DTOBase>();
+		for(DTOBase obj : userListByCodeName){
+			UserDTO user = (UserDTO) obj;
+			StudentDTO student = (StudentDTO) DTOUtil.getObjByCode(studentList, user.getCode());
+			newStudentList.add(setStudent(student, user));
+		}
+		return newStudentList;
+	}
+	
+	public static List<DTOBase> getStudentListByAcademicProgram(List<DTOBase> userList, List<DTOBase> studentListByAcademicProgramCode){
+		List<DTOBase> newStudentList = new ArrayList<DTOBase>();
+		for(DTOBase userObj : studentListByAcademicProgramCode){
+			StudentDTO student = (StudentDTO) userObj;
+			UserDTO user = (UserDTO) DTOUtil.getObjByCode(userList, student.getCode());
+			if(user!=null && !StringUtil.isEmpty(user.getCpNumber())){
+				newStudentList.add(setStudent(student, user));
+			}
+		}
+		return newStudentList;
+	}
+	
+	public static List<DTOBase> getStudentListByAcademicProgram(List<DTOBase> studentList, String academicProgramCode){
+		List<DTOBase> newStudentList = new ArrayList<DTOBase>();
+		for(DTOBase studentObj : studentList){
+			StudentDTO student = (StudentDTO) studentObj;
+			if(student.getAcademicProgram().getCode().equalsIgnoreCase(academicProgramCode)){
+				newStudentList.add(student);
+			}
+		}
+		return newStudentList;
+	}
+	
+	
+	
+	public static StudentDTO getStudent(DTOBase obj){
+		StudentDTO student = (StudentDTO) obj;
+		UserDTO user = new UserDAO().getUserByCode(student.getCode());
+		return setStudent(student, user);
+	}
+	
+	public static String getProfilePicture(StudentDTO student){
+		String pict = !StringUtil.isEmpty(student.getProfilePict())?student.getProfilePict():" ";
+		return "<img src='" + pict + "' class='thumbnail' width='70' height='70' />";
+	}
+	
+	*/
+}
